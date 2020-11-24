6de8fafa1ec70f9c21ccea5585cbfbc2887553f3
Rao Fahad <raofahad046@gmail.com>
2020-11-24 12:45:54 +0500

    updated room.js by doing assignment of state

--- src/Room.js
+++ src/Room.js
@@ -9,23 +9,45 @@ function Room() {
     let [islit, setlit] = useState(true);
 
     let [age, setAge] = useState(23);
+
+    let [temp, setTemp] = useState(72);
+    
     
     function updateLit(){
         console.log('button clicked');
         // islit = !islit;
         setlit(!islit);
     }
+
+    function lightOn(){
+        console.log('light is on')
+        setlit(true); 
+    }
     
+
+    function lightOff(){
+        console.log('light is off')
+        setlit(false); 
+    }
+
     return (
     // <div className={"room " + (islit ? "lit":"dark")}>
     <div className={`room ${islit? "lit":"dark"}`}>
       This room is {islit ? 'lit' : 'dark'}
       <br />
-      <button onClick={updateLit}>Toggle light</button>
+    <button onClick={updateLit}>{islit? "lit":"dark"}</button>
       <br />
       Age is {age}
       <br />
       <button onClick={() => setAge(++age)}>Increase age</button>
+      <br />
+      <button onClick={lightOn}>light on</button>
+      <button onClick={lightOff}>light off</button>
+      <br />
+      <button onClick={() => setTemp(++temp)}><h4>+</h4></button>
+      temperature is {temp}
+      <button onClick={() => setTemp(--temp)}><h4>-</h4></button>
+      <br />
     </div>
   );
 }
